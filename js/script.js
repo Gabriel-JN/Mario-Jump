640bbd06e51681ac4c2b78d645384cce40273c7a
Gabriel-JN <gabriel.jorge19999@gmail.com>
2022-10-28 23:49:55 -0300

    adding Mario collision to the pipe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 const mario = document.querySelector('.mario'); /* getting the class "mario" through a css selector */
-
+const pipe = document.querySelector('.pipe');  /* getting the class "pipe" through a css selector */
 const jump = () => {
     mario.classList.add('jump'); /* adding the jump class in the Mario image */
+    
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500); /* function to remove the jump class, after 
     500 ms. this function allows Mario to jump again and not just 1 time */
 }
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft; /* "offsetLeft" to access the left shift property */
+     /* getting the position left of the pipe class */
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); /* getting the bottom property and and formatting the obtained data to be without the "px".
+    The "+" try to convert a string to number */
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+    } /* if the distance from mario to the pipe is less than 120px, then the animation will stop and the property left will be the Mario position */
+
+}, 10);
 document.addEventListener('keydown', jump); /* adding a event to execute the function "jump"
 after the user press a key */
